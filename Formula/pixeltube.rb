d0c126b2823e0fbc8d76950905ef64d5c499ab3d
Maw <shinji.256@gmail.com>
2026-03-31 17:52:02 -0600

    pixeltube 0.0.3

--- Formula/pixeltube.rb
+++ Formula/pixeltube.rb
@@ -1,8 +1,8 @@
 class Pixeltube < Formula
   desc "Terminal-based YouTube browser and pixel art video player"
   homepage "https://github.com/mawmawmaw/pixeltube"
-  url "https://github.com/mawmawmaw/pixeltube/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "a1e932509c1b779f955c50c9cc610cfa3f4075e75b7ce585957059c9858c0487"
+  url "https://github.com/mawmawmaw/pixeltube/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "4dcbd7c6e08ab160ab05d58bf97becbc5dbc6631ac073b86805d06a8bed3fd68"
   license "MIT"
 
   depends_on "node"
@@ -10,9 +10,11 @@ class Pixeltube < Formula
   depends_on "yt-dlp"
 
   def install
-    system "npm", "install", "--omit=dev", "--ignore-scripts"
+    system "npm", "install"
+    system "npm", "run", "build"
+    system "npm", "prune", "--omit=dev"
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/pixeltube.js" => "pixeltube"
+    bin.install_symlink libexec/"dist/bin/pixeltube.js" => "pixeltube"
   end
 
   test do
