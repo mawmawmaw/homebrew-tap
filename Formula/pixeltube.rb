d0e455506007085844ae170e7781a9aec592b3c0
Maw <shinji.256@gmail.com>
2026-03-31 15:59:32 -0600

    Update pixeltube sha256 for amended v0.0.2 tag

--- Formula/pixeltube.rb
+++ Formula/pixeltube.rb
@@ -2,7 +2,7 @@ class Pixeltube < Formula
   desc "Terminal-based YouTube browser and pixel art video player"
   homepage "https://github.com/mawmawmaw/pixeltube"
   url "https://github.com/mawmawmaw/pixeltube/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "71e6fc5d86200326a0037e14e6e9dfac3ec6cd318d8e443cfd20da4f73b8c204"
+  sha256 "a1e932509c1b779f955c50c9cc610cfa3f4075e75b7ce585957059c9858c0487"
   license "MIT"
 
   depends_on "node"
